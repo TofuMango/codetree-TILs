19e179f57bdc7f101bbe4cdd8234d23e81350e3d
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-03-27 23:23:23 +0900

    Title: 배열의 합, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/배열의 합/sum-of-array.py
+++ 240327/배열의 합/sum-of-array.py
@@ -1,5 +1,14 @@
-n = 4
-for a in range(n):
-    arr = list(map(int, input().split()))
-    sum_val = sum(arr)
-    print(sum_val)
+# n = 4
+# for a in range(n):
+#     arr = list(map(int, input().split()))
+#     sum_val = sum(arr)
+#     print(sum_val)
+
+# 2차원 배열 사용, 각줄마다 정수 입력받음
+arr_2d = [
+    list(map(int, input().split()))
+    for a in range(4)
+]
+
+for i in range(4):
+    print(sum(arr_2d[i]))
