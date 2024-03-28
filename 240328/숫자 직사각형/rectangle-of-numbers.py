321d27eb1d5d992782908799e99a1ca748a50635
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-03-28 22:14:13 +0900

    Title: 숫자 직사각형, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240328/숫자 직사각형/rectangle-of-numbers.py
@@ -0,0 +1,51 @@
+# 0으로 채워져 있는 배열 만들기
+# n = 4
+# arr_2d = [
+#     [0 for _ in range(n)]
+#     for _ in range(n)
+# ]
+# print(arr_2d)
+
+#n*m 사이즈의 격자
+# n, m = 4,5
+# arr_2d = [
+#     [0 for _ in range(n)]
+#     for _ in range(n)
+# ]
+# print(arr_2d)
+
+# 2차원 배열 출력
+# n = 4
+# arr_2d = [[1, 2, 3, 4], [7, 8, 9, 10], [11, 12, 13, 14], [15, 16, 17, 18]]
+# for i in range(n):
+#     for j in range(n):
+#         print(arr_2d[i][j], end=" ")
+#     print()
+
+# 리스트 내 원소 바로 접근
+# n = 4
+# arr_2d = [[1, 2, 3, 4], [7, 8, 9, 10], [11, 12, 13, 14], [15, 16, 17, 18]]
+# # ex) arr_2d 내부의 [1,2,3,4] 
+# for row in arr_2d:
+#     # [1,2,3,4] 내부의 요소 조회
+#     for elem in row:
+#         print(elem, end=" ")
+#     print()
+
+n = list(map(int, input().split()))
+arr_2d = [
+    [0 for _ in range(n[1])]
+    for _ in range(n[0])
+]
+
+num = 1
+for i in range(n[0]):
+    for j in range(n[1]):
+        arr_2d[i][j] = num
+        num += 1
+
+# 출력
+for row in arr_2d:
+    for elem in row:
+        print(elem, end=" ")
+    print()
