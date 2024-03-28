1037076f9b42caba6d08bd76ba8479b2221c64c6
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-03-28 23:56:14 +0900

    Title: 두 개의 격자 비교하기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240328/두 개의 격자 비교하기/compare-two-grid.py
+++ 240328/두 개의 격자 비교하기/compare-two-grid.py
@@ -10,16 +10,23 @@ arr_2 = [
     for _ in range(n)
 ]
 
-arr_3 = [
-    [0 for _ in range(m)]
-    for _ in range(n)
-]
+# 배열 저장하는법 1
+# arr_3 = [
+#     [0 for _ in range(m)]
+#     for _ in range(n)
+# ]
 
-for i in range(n):
-    for j in range(m):
-        # 같으면 0, 그렇지 않다면 1
-        if arr_1[i][j] != arr_2[i][j]:
-            arr_3[i][j] = 1
+# for i in range(n):
+#     for j in range(m):
+#         # 같으면 0, 그렇지 않다면 1
+#         if arr_1[i][j] != arr_2[i][j]:
+#             arr_3[i][j] = 1
+
+# 배열 저장하는법 2
+arr_3 = [
+    [1 if arr_1[i][j] != arr_2[i][j] else 0 for j in range(m)]
+    for i in range(n)
+]
 
 for row in arr_3:
     for result in row:
