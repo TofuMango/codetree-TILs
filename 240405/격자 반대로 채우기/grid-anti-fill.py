b7836668d528ed7ee4cc6419db628201fae11a66
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-04-05 17:22:42 +0900

    Title: 격자 반대로 채우기, Time: 133ms, Memory: 73MB, Status: Passed - CodeTree

--- 240405/격자 반대로 채우기/grid-anti-fill.py
+++ 240405/격자 반대로 채우기/grid-anti-fill.py
@@ -7,18 +7,23 @@ answer = [
 
 count = 1
 
-# n = 4 일때, 4-1=3, 3,2,1 열만 해당됨.
-# 근데 n, 0, -1 하면, 4 3 2 1임. 범위 벗어남. 0 포함 하고 싶으면 -1 써줘야함.
+# 오른쪽 아래에서 시작
 for i in range(n-1, -1, -1):
-    if i % 2 == 0:
-        for j in range(n):
-            answer[j][i] = count
-            count += 1
-    else:
+    # 5-1-4 = 0 %2 == 0임
+    # 근데 만약 i%2 == 0 으로 했다면 3 % 2 = 0이 아님. 그래서 else가 됐음
+    # 4-1-3 = 0 % 2 == 0 이지..뭔짓을 해도 시작은 아래에서 위로 채울 수 밖에 없음.
+    if (n-1-i) % 2 == 0:
+        # 열의 인덱스가 짝수일 때 아래에서 위로 채우기
         for j in range(n-1, -1, -1):
             answer[j][i] = count
             count += 1
+    else:
+        # 열의 인덱스가 홀수일 때 위에서 아래로 채우기
+        for j in range(n):
+            answer[j][i] = count
+            count += 1
 
+# 결과 출력
 for row in answer:
     for col in row:
         print(col, end=' ')
