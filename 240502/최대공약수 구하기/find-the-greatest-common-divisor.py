ea3b307481f60f4f150e482fcca153ffa7cbfcf9
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-02 18:23:19 +0900

    Title: 최대공약수 구하기, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240502/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,9 @@
+# 입력
+n, m = tuple(map(int, input().split()))
+
+# 최대공약수 구하는 함수
+def gcd(a, b):
+    for i in range(min(a, b), 0, -1):
+        if a % i == 0 and b % i == 0:
+            return print(i)
+gcd(n, m)
