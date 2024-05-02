068609b214d20b7dfe7e2782f88717f23bd591d0
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-02 18:24:20 +0900

    Title: 최대공약수 구하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240502/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240502/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -5,5 +5,6 @@ n, m = tuple(map(int, input().split()))
 def gcd(a, b):
     for i in range(min(a, b), 0, -1):
         if a % i == 0 and b % i == 0:
-            return print(i)
+            gcd = i
+    print(gcd)
 gcd(n, m)
