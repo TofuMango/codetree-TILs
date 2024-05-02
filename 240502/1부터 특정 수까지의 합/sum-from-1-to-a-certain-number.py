d4393a46f8d348275ac03856f6fd02265cb80094
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-02 20:09:16 +0900

    Title: 1부터 특정 수까지의 합, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240502/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.py
+++ 240502/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.py
@@ -12,3 +12,17 @@ def div(n):
 # 출력
 total = div(N)
 print(total)
+
+# # 변수 선언 및 입력:
+# n = int(input())
+
+
+# def sum_n(n):
+#     sum_val = 0
+#     for i in range(1, n + 1):
+#         sum_val += i
+    
+#     return sum_val // 10
+
+
+# print(sum_n(n))
