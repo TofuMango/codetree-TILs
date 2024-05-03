aef9fd5056cd262b37dc1c8e94bfb0e55ddfc99f
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-03 17:49:37 +0900

    Title: 함수를 이용한 소수 판별, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240503/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -3,10 +3,12 @@ a, b = tuple(map(int, input().split()))
 def findPrimeNum(n):
     if n == 1:
         return False
+
     for i in range(2, n):
         if n % i == 0:
             return False
-    return n
+
+    return True
 
 hab = 0
 for i in range(a, b+1):
