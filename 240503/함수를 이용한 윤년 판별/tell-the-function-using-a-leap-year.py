7ac184d6c7d0de2415bf5bd94a94d0e02328420d
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-03 18:35:50 +0900

    Title: 함수를 이용한 윤년 판별, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240503/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,10 +1,10 @@
 y = int(input())
 
 def year(n):
-    if y % 4 == 0:
-        return True
     if y % 100 == 0 and y % 400 != 0:
         return False
+    if y % 4 == 0:
+        return True
     return False
 
 if year(y):
