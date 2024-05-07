19bd2e894fe80fa68fe2f932bedb725c98462d00
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-07 20:38:55 +0900

    Title: 중앙값 계산 2, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/중앙값 계산 2/get-median-2.py
+++ 240507/중앙값 계산 2/get-median-2.py
@@ -1,7 +1,7 @@
 n = int(input())
 numList = list(map(int, input().split()))
 
-for i in range(n+1):
-    if i % 2 != 0:
-        tem = sorted(numList[0:n])
-        print(numList[i//2], end=' ')
+for i in range(n):
+    if i % 2 == 0:
+        tem = sorted(numList[0:i+1])
+        print(tem[i//2], end=' ')
