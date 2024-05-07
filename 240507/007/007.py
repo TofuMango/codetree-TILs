8b312957696ccb6f698b7ae29b5c940965a54a37
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-07 22:32:55 +0900

    Title: 007, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/007/007.py
+++ 240507/007/007.py
@@ -1,24 +1,17 @@
-# class Student:
-#     def __init__(self, kor, eng, math):
-#         # self는 student 를 지칭...
-#         self.kor = kor
-#         self.eng = eng
-#         self.math = math
-
-# student1 = Student(90, 80, 90)
-# print(student1.kor)  # 90
-# print(student1.eng)  # 80
-# print(student1.math) # 90
-
-
+# 클래스 선언
 class secretcode:
     def __init__(self, code, meet, time):
         self.code = code
         self.meet = meet
         self.time = time
 
+# 변수 선언 
 cd, mt, t = input().split()
+
+# 객체 생성
 secretC = secretcode(cd, mt, t)
+
+# 출력
 print("secret code :", secretC.code)
 print("meeting point :", secretC.meet)
 print("time :", secretC.time)
