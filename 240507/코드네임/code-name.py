eb838df8e4bae958912265f7e27ed5d1c98d3405
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-07 23:55:19 +0900

    Title: 코드네임, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/코드네임/code-name.py
+++ 240507/코드네임/code-name.py
@@ -1,17 +1,41 @@
-class CodeName:
-    def __init__(self, codename, score):
-        self.codename = codename
-        self.score = int(score)
+# 클래스 선언
+class User:
+    def __init__(self, code_name, score):
+        self.code_name = code_name
+        self.score = score
 
-CN = []
+
+# 변수 선언 및 입력
+users = []
+
 for _ in range(5):
-    codename, score = (input().split())
-    CN.append(CodeName(codename, score))
+    code_name, score = tuple(input().split())
+    users.append(User(code_name, int(score)))
 
-minScore = 1000000
-minCodename = ""
-for i in CN:
-    if i.score < minScore:
-        minScore = i.score
-        minCodename = i.codename
-print(minCodename, minScore)
+
+# 최소 점수를 갖는 유저 찾기
+min_idx = 0
+for i in range(1, 5):
+    if users[min_idx].score > users[i].score:
+        min_idx = i
+
+# 출력
+print(users[min_idx].code_name, users[min_idx].score)
+
+# class CodeName:
+#     def __init__(self, codename, score):
+#         self.codename = codename
+#         self.score = int(score)
+
+# CN = []
+# for _ in range(5):
+#     codename, score = (input().split())
+#     CN.append(CodeName(codename, score))
+
+# minScore = 1000000
+# minCodename = ""
+# for i in CN:
+#     if i.score < minScore:
+#         minScore = i.score
+#         minCodename = i.codename
+# print(minCodename, minScore)
