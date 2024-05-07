abcf4c3c7f59729f375c606748e818018a896e47
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-07 19:09:46 +0900

    Title: k번째로 신기한 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240507/k번째로 신기한 문자열/kth-special-string.py
+++ 240507/k번째로 신기한 문자열/kth-special-string.py
@@ -1,5 +1,6 @@
+# 수정ver
 n, k, T = input().split()
-
+k = int(k)
 # 문자열들 입력받아서 배열에 저장
 arrWord = [
     list(map(str, input().split()))
@@ -15,4 +16,24 @@ for i in arrWord:
         if j[0:2] == T:
             tList.append(j)
 
-print(tList[int(k)-1])
+print(tList[k-1])
+
+# 런타임 에러ver
+# n, k, T = input().split()
+
+# # 문자열들 입력받아서 배열에 저장
+# arrWord = [
+#     list(map(str, input().split()))
+#     for _ in range(int(n))
+# ]
+# # 배열 사전순 정렬
+# arrWord.sort()
+# # ap만 들은 list만들기
+# tList = []
+# # 문자열T로 시작하는 단어들을 배열ap에 저장
+# for i in arrWord:
+#     for j in i:
+#         if j[0:2] == T:
+#             tList.append(j)
+
+# print(tList[int(k)-1])
