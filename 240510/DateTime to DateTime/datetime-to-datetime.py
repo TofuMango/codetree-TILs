37ff57e65997d2b9cc08bad7990701c7ba41f5e4
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-10 18:02:50 +0900

    Title: DateTime to DateTime, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240510/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,24 @@
+day, hour, miniute = 11, 11, 11
+a, b, c = tuple(map(int, input().split()))
+elapsed_time = 0
+
+while True:
+    if a < day and b < hour and c < miniute:
+        print(-1)
+        break 
+    if day == a and hour == b and miniute == c:
+        break
+    
+    elapsed_time += 1
+    miniute += 1
+
+    if miniute >= 60:
+        hour += 1
+        miniute = 0
+
+    if hour >= 24:
+        day += 1
+        hour = 0
+        miniute = 0
+    
+print(elapsed_time)
