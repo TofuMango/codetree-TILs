9ec2c545d6a32621c8522306cf980c3a1e4fe351
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-10 17:41:10 +0900

    Title: Date to Date, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240510/Date to Date/date-to-date.py
@@ -0,0 +1,15 @@
+month, day, end_month, end_day = tuple(map(int, input().split()))
+elapsed_day = 0
+# 윤년X 기준 2월은 28일까지
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+while True:
+    if month == end_month and day == end_day:
+        break
+    elapsed_day += 1
+    day += 1
+
+    if day> num_of_days[month]:
+        month+=1
+        day = 1
+pritn(elapsed_day)
