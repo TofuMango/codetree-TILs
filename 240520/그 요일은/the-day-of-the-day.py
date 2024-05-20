76af1f741f68c2c146cfdd8795a39c24897a0136
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-20 19:54:15 +0900

    Title: 그 요일은, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240520/그 요일은/the-day-of-the-day.py
+++ 240520/그 요일은/the-day-of-the-day.py
@@ -1,30 +1,40 @@
-# 날짜
+# 날짜와 월 입력 받기
+m1, d1, m2, d2 = map(int, input().split())
+day_input = input()
+
+# 요일 리스트
 day_list = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-# 월
-month = [31,29,31,30,31,30,31,31,30,31,30,31]
-# 입력
-m1, d1, m2, d2 = tuple(map(int, input().split()))
-day = input()
-# 횟수세기
-cnt = 0
-index = 0
-# day를 인덱스화 시키기
-for i in range(len(day_list)):
-    if day == day_list[i]:
-        index = i
-while True:
-    # 만약 d1이 31, 30일 이러면 다음달로 넘어가
-    if d1 == month[m1-1]:
+
+# 각 월의 일수
+month_days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# 시작 요일을 인덱스로 변환
+day_index = day_list.index(day_input)
+
+# 누적 일수 계산
+total_days = 0
+for month in range(m1 - 1):
+    total_days += month_days[month]
+total_days += d1
+
+# 카운트와 현재 요일 인덱스 초기화
+count = 0
+current_day_index = day_index
+
+# 날짜 세기 시작
+while not (m1 == m2 and d1 == d2):
+    if current_day_index == day_index:
+        count += 1
+    # 다음 날로 이동
+    d1 += 1
+    if d1 > month_days[m1 - 1]:
         d1 = 1
         m1 += 1
-    # 아니면 d1을 1씩더하고
-    else:
-        d1 += 1
-        # 만약 날짜가 Sat라면 cnt += 1하기
-        if d1%7 == index or d1 == index:
-            cnt += 1
-    if d1 == d2 and m1 == m2:
-        if d1%7 == index or d1 == index:
-            cnt += 1
-        print(cnt+1)
-        break
+    # 요일 인덱스 업데이트
+    current_day_index = (current_day_index + 1) % 7
+
+# 마지막 날짜 포함 여부 확인
+if current_day_index == day_index:
+    count += 1
+
+print(count)
