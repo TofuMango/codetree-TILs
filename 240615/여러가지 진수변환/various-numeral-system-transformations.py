49ac2de4971e06d9a52d43ff32977b68d1ebab27
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-06-15 00:11:58 +0900

    Title: 여러가지 진수변환, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240615/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,10 @@
+n, b = tuple(map(int, input().split()))
+arr = []
+while True:
+    if n<2:
+        arr.append(n)
+        break
+    arr.append(n%b)
+    n //= b
+for res in arr[::-1]:
+    print(res, end='')
