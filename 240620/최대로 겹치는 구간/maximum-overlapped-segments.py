37b186bc6439f2a13e8f73dd990ab747aafd3bcc
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-06-20 19:40:54 +0900

    Title: 최대로 겹치는 구간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240620/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240620/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,34 +1,22 @@
-# n = int(input())
-# res = [0] * 100
-# for i in range(n):
-#     a, b = tuple(map(int, input().split()))
-#     if a <= 0:
-#         a += abs(a)
-#         b += abs(a)
-#         for j in range(a-1,b-1):
-#             res[j]+=1
-#     for j in range(a-1,b-1):
-#         res[j]+=1
-# print(max(res))
+OFFSET = 100
+# 100을 더하고 진행하기때문에 [0,200]내의 최댓값을 구하게됨 그래서 max값을 200으로 줌
+MAX_R = 200
+# 입력
 n = int(input())
-events = []
+seg = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+check = [0] * (MAX_R+1)
+# offset을 적용하여 음수인 수를 양수로 변환해줌
+for x1, x2 in segments:
+    x1, x2 = x1+OFFSET, x2+OFFSET
+    # 구간 칠해주기
+    # 만약 [1, 5]였다 하면 구간은 1-2, 2-3, 3-4, 4-5가 칠해져야 하므로 1,2,3,4 에 카운트해줘야함 그러니까 x1, x2-1 이 되도록 해주면 됨
+    # for 문이므로 x1 ~ x2-1임
+    for i in range(x1, x2):
+        check[i]+=1
 
-# 이벤트 리스트에 시작과 끝점을 기록
-for _ in range(n):
-    a, b = map(int, input().split())
-    events.append((a, 1))  # 선분 시작
-    events.append((b, -1))  # 선분 끝
-
-# 이벤트를 정렬합니다. 시작점을 우선으로 정렬하고, 동일한 경우 끝점을 그 다음으로 정렬
-events.sort()
-
-max_overlap = 0
-current_overlap = 0
-
-# 이벤트를 순회하며 겹치는 선분 수를 계산
-for x, event in events:
-    current_overlap += event
-    if current_overlap > max_overlap:
-        max_overlap = current_overlap
-
-print(max_overlap)
+# 최댓값 구하기
+max_num = max(check)
+print(max_num)
