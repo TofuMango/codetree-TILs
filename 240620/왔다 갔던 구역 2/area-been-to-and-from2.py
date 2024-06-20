c9f6339621dd8182159111ccb70144d9dae91f8f
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-06-20 22:22:33 +0900

    Title: 왔다 갔던 구역 2, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240620/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,26 @@
+offerset = 100
+max_space = 200
+n = int(input())
+segment = [
+    tuple(input().split())
+    for _ in range(n)
+]
+res = [0] * (max_space+1)
+# res[100]의 위치부터 시작
+for x, y in segment:
+    x = int(x)
+    # 만약 y가 R이라면 
+    if y == 'R':
+        for i in range(x):
+            res[offerset+i]+=1
+        offerset+=1
+            
+    if y == 'L':
+        for i in range(x):
+            res[offerset-i]+=1
+        offerset-=1
+cnt = 0
+for r in res:
+    if r >= 2:
+        cnt += 1
+print(cnt)
