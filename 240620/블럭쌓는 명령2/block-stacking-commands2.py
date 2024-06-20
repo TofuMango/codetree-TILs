67518b9316e7ff7dbc867f418b565be10c5429ba
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-06-20 18:16:13 +0900

    Title: 블럭쌓는 명령2, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240620/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240620/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,11 +1,10 @@
 n, k = tuple(map(int, input().split()))
 # max값을 구하기 위한 1차원 배열 선언 후 초기화
 res = []
-for i in range(n):
-    res.append(0)
+res = [0] * n
 # k번 만큼 반복
 for i in range(k):
     a, b = tuple(map(int, input().split()))
     for i in range(a-1, b):  # 0-indexed 배열에 맞게 범위 조정
-        block_count[i] += 1
+        res[i] += 1
 print(max(res))
