429408445e7b7a6957447490101130aabe8a7778
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-06-20 19:41:19 +0900

    Title: 최대로 겹치는 구간, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240620/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240620/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -9,7 +9,7 @@ seg = [
 ]
 check = [0] * (MAX_R+1)
 # offset을 적용하여 음수인 수를 양수로 변환해줌
-for x1, x2 in segments:
+for x1, x2 in seg:
     x1, x2 = x1+OFFSET, x2+OFFSET
     # 구간 칠해주기
     # 만약 [1, 5]였다 하면 구간은 1-2, 2-3, 3-4, 4-5가 칠해져야 하므로 1,2,3,4 에 카운트해줘야함 그러니까 x1, x2-1 이 되도록 해주면 됨
