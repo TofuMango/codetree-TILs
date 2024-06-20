307949f9cca897efa63335b8efe1d6ca59e4d960
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-06-20 22:26:11 +0900

    Title: 왔다 갔던 구역 2, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240620/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240620/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,26 +1,34 @@
-offerset = 300
-max_space = 600
+# 입력 받기
 n = int(input())
-segment = [
-    tuple(input().split())
-    for _ in range(n)
-]
-res = [0] * (max_space+1)
-# res[100]의 위치부터 시작
-for x, y in segment:
-    x = int(x)
-    # 만약 y가 R이라면 
-    if y == 'R':
-        for i in range(1, x):
-            res[offerset+i]+=1
-        offerset+=x
-            
-    if y == 'L':
-        for i in range(1, x):
-            res[offerset-i]+=1
-        offerset-=x
-cnt = 0
-for r in res:
-    if r >= 2:
-        cnt += 1
-print(cnt)
+commands = [input().split() for _ in range(n)]
+
+# 위치 추적을 위한 딕셔너리 초기화
+position_count = {}
+current_position = 0
+
+# 명령어 처리
+for command in commands:
+    distance = int(command[0])
+    direction = command[1]
+    
+    if direction == 'R':
+        for _ in range(distance):
+            current_position += 1
+            if current_position in position_count:
+                position_count[current_position] += 1
+            else:
+                position_count[current_position] = 1
+                
+    elif direction == 'L':
+        for _ in range(distance):
+            current_position -= 1
+            if current_position in position_count:
+                position_count[current_position] += 1
+            else:
+                position_count[current_position] = 1
+
+# 2번 이상 지나간 영역의 크기 계산
+count_overlapping_areas = sum(1 for count in position_count.values() if count >= 2)
+
+# 결과 출력
+print(count_overlapping_areas)
