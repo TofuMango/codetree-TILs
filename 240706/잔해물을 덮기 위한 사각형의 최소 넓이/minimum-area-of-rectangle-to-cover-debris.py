ba5f8a0953c54be41b8ace7596654763010b2941
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-07-06 21:10:30 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240706/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240706/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -4,47 +4,25 @@ square = [
     tuple(map(int, input().split()))
     for _ in range(2)
 ]
-check = [
-    [0] * (maxSpace + 1)
-    for _ in range(maxSpace + 1)
-]
 
-# 사각형의 좌표 변환 및 매핑
-for index, (x1, y1, x2, y2) in enumerate(square, start=1):
+# 좌표 변환
+transformed_squares = []
+for (x1, y1, x2, y2) in square:
     x1, y1 = offset + x1, offset + y1
     x2, y2 = offset + x2, offset + y2
-    for i in range(x1, x2):
-        for j in range(y1, y2):
-            check[i][j] = index
+    transformed_squares.append((x1, y1, x2, y2))
 
-# 겹치는 영역의 최대 너비와 높이를 계산
-x_max = 0
-y_max = 0
+# 교차 영역 계산
+def calculate_intersection_area(sq1, sq2):
+    x1 = max(sq1[0], sq2[0])
+    y1 = max(sq1[1], sq2[1])
+    x2 = min(sq1[2], sq2[2])
+    y2 = min(sq1[3], sq2[3])
 
-# x_max 계산
-for i in range(0, maxSpace + 1):
-    tmp = 0
-    for j in range(0, maxSpace + 1):
-        if check[i][j] == 1 and check[i][j] == check[i][j]:
-            tmp += 1
-        else:
-            if x_max < tmp:
-                x_max = tmp
-            tmp = 0
-    if x_max < tmp:
-        x_max = tmp
+    if x1 < x2 and y1 < y2:
+        return (x2 - x1) * (y2 - y1)
+    else:
+        return 0
 
-# y_max 계산
-for j in range(0, maxSpace + 1):
-    tmp = 0
-    for i in range(0, maxSpace + 1):
-        if check[i][j] == 1 and check[i][j] == check[i][j]:
-            tmp += 1
-        else:
-            if y_max < tmp:
-                y_max = tmp
-            tmp = 0
-    if y_max < tmp:
-        y_max = tmp
-
-print(x_max * y_max)
+area = calculate_intersection_area(transformed_squares[0], transformed_squares[1])
+print(area)
