f3a14b59260f781da3b35883d33af66282265a01
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-07-06 18:03:26 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240706/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,31 @@
+maxSpace = 100
+result = [0] * (2*maxSpace+1)
+cur = maxSpace
+w, b = 0, 0
+n = int(input())
+for i in range(n):
+    x, p = tuple(input().split())
+    x = int(x)
+    if p == 'R':
+        while x > 0:
+            result[cur] = 1
+            x-=1
+
+            if x:
+                cur += 1
+    else :
+        while x > 0:
+            result[cur] = 2
+            x-=1
+
+            if x:
+                cur -= 1
+
+for j in range(2*maxSpace+1):
+    if result[j] == 1:
+        b += 1
+    elif result[j] == 2:
+        w += 1
+    else:
+        continue
+print(w, b)
