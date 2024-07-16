a447c40ab19313f0005c3d28a6671070df96edf6
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-07-16 19:38:50 +0900

    Title: 연속되는 수 3, Time: 124ms, Memory: 28MB, Status: Passed - CodeTree

--- 240716/연속되는 수 3/continuous-number3.py
+++ 240716/연속되는 수 3/continuous-number3.py
@@ -1,15 +1,23 @@
+# 변수 선언 및 입력
 n = int(input())
 arr = [
-    int(input())
-    for _ in range(n)
+	int(input())
+	for _ in range(n)
 ]
 
-ans, cnt = 1, 1  # 첫 번째 숫자는 항상 포함되므로 1로 초기화
-for i in range(1, n):  # 두 번째 숫자부터 시작
-    if (arr[i] > 0 and arr[i - 1] > 0) or (arr[i] < 0 and arr[i - 1] < 0):  # 부호가 동일한지 확인
-        cnt += 1
-    else:
-        cnt = 1  # 부호가 다르면 cnt를 1로 초기화
-    ans = max(ans, cnt)  # 최대 길이를 갱신
+# 연속하여 동일한 숫자가 나오는 횟수 구함
+# 그 중 최댓값을 갱신
+ans, cnt = 0, 0
+for i in range(n):
+	# Case 1
+	# 두번째 요소부터 비교하기, 부호가 일치하면 양수가 됨(음음양 -> 음수/ 양양음 -> 음수)
+	if i >= 1 and arr[i] * arr[i - 1] > 0:
+		cnt += 1
+	# Case 2
+	# 
+	else:
+		cnt = 1
+	
+	ans = max(ans, cnt)
 
 print(ans)
