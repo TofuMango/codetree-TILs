86495c9abb9ffa0860dd4bbafdaf4d519ddd5bdc
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-07-19 17:24:40 +0900

    Title: 만나는 그 순간, Time: 175ms, Memory: 40MB, Status: Passed - CodeTree

--- 240719/만나는 그 순간/the-moment-we-meet.py
+++ 240719/만나는 그 순간/the-moment-we-meet.py
@@ -1,48 +1,38 @@
-n, m = map(int, input().split())
+MAX_T = 1000000
 
-# A와 B의 이동 경로를 저장할 리스트
-dirA = [tuple(input().split()) for _ in range(n)]
-dirB = [tuple(input().split()) for _ in range(m)]
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+pos_a, pos_b = [0] * (MAX_T + 1), [0] * (MAX_T + 1)
 
-# 이동 경로를 기록할 딕셔너리
-positionsA = {}
-positionsB = {}
+# A가 매 초마다 서있는 위치를 기록
+time_a = 1
+# n = a의 이동명령횟수
+for _ in range(n):
+    # d는 방향 t는 초
+    d, t = tuple(input().split())
+    # 초만큼 반복
+    for _ in range(int(t)):
+        # a[현재시간] = a[현재시간-1](즉 이전시간)의 값에다가 d가 R이라면 +1더해주고, L이라면 -1
+        pos_a[time_a] = pos_a[time_a - 1] + (1 if d == 'R' else -1)
+        # 시간 1증가
+        time_a += 1
 
-# 현재 위치를 저장할 변수
-posA = 0
-posB = 0
+# B가 매 초마다 서있는 위치를 기록
+time_b = 1
+for _ in range(m):
+    d, t = tuple(input().split())
+    for _ in range(int(t)):
+        pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
+        time_b += 1
 
-# 시간 기록용 변수
-timeA = 0
-timeB = 0
-
-# A의 이동 경로 기록
-for direction, num in dirA:
-    num = int(num)
-    for _ in range(num):
-        if direction == 'R':
-            posA += 1
-        elif direction == 'L':
-            posA -= 1
-        timeA += 1
-        positionsA[timeA] = posA
-
-# B의 이동 경로 기록
-for direction, num in dirB:
-    num = int(num)
-    for _ in range(num):
-        if direction == 'R':
-            posB += 1
-        elif direction == 'L':
-            posB -= 1
-        timeB += 1
-        positionsB[timeB] = posB
-
-# 최초로 만나는 시간을 찾기
-meeting_time = -1
-for t in range(1, max(timeA, timeB) + 1):
-    if positionsA.get(t) == positionsB.get(t):
-        meeting_time = t
+# 최초로 만나는 시간
+# -1은 둘이 만나지 않았을때 출력되어야하니까 -1로 초기화해줌
+ans = -1
+# time_a == time_b 걸린시간은 서로 동일함
+for i in range(1, time_a):
+    # 둘이 서로 같은시간에 같은값이 되는 순간 ans를 i 초로 저장
+    if pos_a[i] == pos_b[i]:
+        ans = i
         break
-
-print(meeting_time)
+        
+print(ans)
