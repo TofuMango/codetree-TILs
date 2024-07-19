1d4ec384f25ca9f2634e0ba790e412d88d47c7c9
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-07-19 18:06:49 +0900

    Title: 벌금은 누구에게, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240719/벌금은 누구에게/who-will-pay.py
+++ 240719/벌금은 누구에게/who-will-pay.py
@@ -1,12 +1,30 @@
+# n, m, k = tuple(map(int, input().split()))
+# manage = [0 for _ in range(n+1)]
+# first = -1
+
+# for i in range(m):
+#     memberIndex = int(input())
+#     manage[memberIndex] += 1
+#     # 이미 횟수에 도달하면 카운팅되지않도록 first==-1조건추가
+#     if k == manage[memberIndex] and first == -1:
+#         first = memberIndex
+# print(first)
+# 변수 선언 및 입력
 n, m, k = tuple(map(int, input().split()))
-manage = [0 for _ in range(n)]
-first = -1
+penalized_person = [
+    int(input())
+    for _ in range(m)
+]
+penalty_num = [0] * (n + 1)
 
-for i in range(m):
-    memberIndex = int(input()) - 1  # 인덱스는 0부터 시작하므로 -1
-    manage[memberIndex] += 1
-    # 이미 횟수에 도달하면 카운팅되지않도록 first==-1조건추가
-    if k == manage[memberIndex] and first == -1:
-        first = memberIndex + 1  # 출력은 1부터 시작하는 인덱스로 하기 위해 +1
+# 각 패널티 횟수를 세서,
+# 최초로 K번 이상 벌칙을 받는 사람을 추적
+ans = -1
+for target in penalized_person:
+    penalty_num[target] += 1
 
-print(first)
+    if penalty_num[target] >= k:
+        ans = target
+        break
+
+print(ans)
