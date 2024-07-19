da7af02fb9b3aebed13aea77c2f98e44064a58e3
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-07-19 18:02:01 +0900

    Title: 벌금은 누구에게, Time: 160ms, Memory: 26MB, Status: Passed - CodeTree

--- 240719/벌금은 누구에게/who-will-pay.py
+++ 240719/벌금은 누구에게/who-will-pay.py
@@ -5,7 +5,8 @@ first = -1
 for i in range(m):
     memberIndex = int(input()) - 1  # 인덱스는 0부터 시작하므로 -1
     manage[memberIndex] += 1
-    if k == manage[memberIndex]:
+    # 이미 횟수에 도달하면 카운팅되지않도록 first==-1조건추가
+    if k == manage[memberIndex] and first == -1:
         first = memberIndex + 1  # 출력은 1부터 시작하는 인덱스로 하기 위해 +1
 
 print(first)
