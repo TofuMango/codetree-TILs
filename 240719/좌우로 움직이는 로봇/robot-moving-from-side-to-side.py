30f06e7f6bf53d594fff7cc4157932ab830d054b
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-07-19 20:32:56 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240719/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,49 @@
+max_space = 20
+# a이동횟수 n, b이동횟수 m 입력받기
+n, m = tuple(map(int, input().split()))
+# A와 B 이동거리 저장할 배열 0으로 초기화
+pos_A, pos_B = [0] * (max_space+1), [0] * (max_space+1)
+
+# A의 현재시간
+A_time = 1
+# A 이동거리 계산
+for _ in range(n):
+    t, d = input().split()
+    for i in range(int(t)):
+        if d == 'R':
+            pos_A[A_time] = pos_A[A_time-1] + 1
+            A_time += 1
+        else:
+            pos_A[A_time] = pos_A[A_time-1] - 1
+            A_time += 1
+
+# B 이동거리
+B_time = 1
+for _ in range(m):
+    t, d = input().split()
+    for i in range(int(t)):
+        if d == 'R':
+            pos_B[B_time] = pos_B[B_time-1] + 1
+            B_time += 1
+        else:
+            pos_B[B_time] = pos_B[B_time-1] - 1
+            B_time += 1
+# A, B중 최대시간에 맞춰서 배열의 남은공간에 마지막 값 채워주기
+maxtime = max(A_time, B_time)
+if A_time > B_time:
+    maxtime = A_time
+    for i in range(B_time, maxtime+1):
+        pos_B[B_time] = pos_B[B_time-1]
+        B_time +=1
+else:
+    maxtime = B_time
+    for i in range(A_time, maxtime+1):
+        pos_A[A_time] = pos_A[A_time-1]
+        A_time +=1
+
+ans = 0
+for i in range(1, max(A_time, B_time)):
+    # 현재시간의 이동거리가 일치하면서, 이전시간의 거리는 일치하지않을때만 +1
+    if pos_A[i] == pos_B[i] and pos_A[i-1] != pos_B[i-1]:
+        ans += 1
+print(ans)
