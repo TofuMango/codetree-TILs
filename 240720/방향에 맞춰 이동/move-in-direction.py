b55c865a816cf8bbd41e628150eeaffb07da193c
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-07-20 19:02:03 +0900

    Title: 방향에 맞춰 이동, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240720/방향에 맞춰 이동/move-in-direction.py
+++ 240720/방향에 맞춰 이동/move-in-direction.py
@@ -1,18 +1,22 @@
 N = int(input())
 x, y = 0, 0
-# 동서남북 정보입력
-dx, dy = [-1, 0, 0, 1], [0, 1, -1, 0]
+
+# 동서남북 방향 정의
+dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
 
 for _ in range(N):
-    d, v = tuple(input().split())
-    if d == 'W':
-        d = 0
-    elif d == 'S':
-        d = 1
+    d, v = input().split()
+    v = int(v)
+    
+    if d == 'E':
+        dir_num = 0
+    elif d == 'W':
+        dir_num = 1
     elif d == 'N':
-        d = 2
-    else:
-        d = 3
-    for _ in range(int(v)):
-        x, y = x + dx[d], y + dy[d]
-print(abs(x), abs(y))
+        dir_num = 2
+    elif d == 'S':
+        dir_num = 3
+    
+    x, y = x + dx[dir_num] * v, y + dy[dir_num] * v
+
+print(x, y)
