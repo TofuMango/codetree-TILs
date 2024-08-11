a9d9434b6d5cc8255d5c70137322d9d7353a54a7
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-11 18:17:36 +0900

    Title: 거울에 레이저 쏘기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240811/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240811/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,100 +1,61 @@
-#               0
-#               ^
-#               | 
-#               v      
-#      3 <--> (/,\) <-->1 
-#               ^
-#               |
-#               v
-#               2
-# 북 남 서 동
-def dir_code23(inputLight): #23은 / 을 2번 3번꼭지점으로 생각하여 분류한 것 입니다.
-    if inputLight == 0: # 들어가는 방향
-        return 1        # 나가는방향이면서 다음 번 거울이 받는 방향
-    elif inputLight == 1:
-        return 0
-    elif inputLight == 2:
-        return 3
-    else:
-        return 2
-def dir_code14(inputLight):
-    if inputLight == 0:
-        return 3
-    elif inputLight == 3:
-        return 0
-    elif inputLight == 1:
-        return 2
-    else:
-        return 1
-
-def move(dir_now): # 반사돼 나오는 빛의 이동경로를 표시한 함수입니다.
-    if dir_now == 0:
-        return [1 ,0]
-    elif dir_now == 1:
-        return [0 ,-1]
-    elif dir_now == 2:
-        return [-1,0]
-    else:
-        return [0,1]
-
-
-
+# 입력
 n = int(input())
-
+# 맵정보 입력
+map = [
+    input()
+    for _ in range(n)
+]
+# 시작위치 입력
+start_num = int(input())
+# 맵 범위 정의
 def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
-
-array = [[0 for i in range(n)] for j in range(n)]      #거울이 들어있는 배열
-arrayLight = [[0 for i in range(n)] for j in range(n)] #반사된 빛이 들어있는 배열
-lightstep = 0 #빛이 몇번 반사됐는지 나타냅니다.
-
-for i in range(n):
-    mirror = input()
-    for j in range(n):
-        array[i][j] = mirror[j]
-
-inLight = int(input())-1
-
-start_dir = inLight // n  # 들어갈 방향
-start_num = inLight % n  # 들어갈 방향에서의 번호
-
-x , y = 0, 0   # 가로 세로
-
-if start_dir == 0: # 위에서 진입할때
-    x = 0
-    y = start_num 
-elif start_dir == 1: # 오른쪽에서 진입할때
-    x = start_num 
-    y = n - 1
-elif start_dir == 2: # 아래에서 진입할때
-    x = n - 1
-    y = n - start_num -1
-else:
-    x = n - start_num -1 #왼쪽에서 진입할때
-    y = 0
-
-dir_now = start_dir 
-
-while True:
-    #일단 들어가면서 불을 킨다
-    lightstep += 1
-    arrayLight[x][y] = lightstep
-
-    if array[x][y] == "/":
-        dir_now = dir_code23(dir_now)
-        dx , dy = move(dir_now)
-        x += dx
-        y += dy
-        #print(dir_now,x , y )
+    return x>=0 and y>=0 and x<n, y<n
+# 시작위치 구하는 함수정의
+def primary(num):
+    # 맵 위쪽에서 시작
+    if num <= n:
+        # x는 1행(보정- 0행) 고정이므로 0
+        # 시작방향의 경우 0이 아래, 1이 왼쪽, 2가 위쪽, 3이 오른쪽
+        return 0, num-1, 0
+    # 맵 오른쪽에서 시작
+    elif num <= 2 * n:
+        # y는 n열고정이므로 n-1
+        return num-n-1, n-1, 1
+    # 맵 아래쪽에서 시작
+    elif num <= 3 * n:
+        # x는 n행 고정이므로 n-1
+        return n-1, n-(num-2*n), 2
     else:
-        dir_now = dir_code14(dir_now)
-        dx , dy = move(dir_now)
-        x += dx
-        y += dy
-        #print(dir_now,x , y )
-    
-    if in_range(x, y)!=True: #만약 빛이 범위 밖으로 빠져나왔다면 출력해줍니다.
-        #print("Break {0} {1}".format(x,y))
-        break
+        # 맵 왼쪽에서 시작 y는 0행고정임
+        return n-(num-3*n), 0, 3
 
-print(lightstep)
+# 다음 위치로 이동하기
+def move(x,y,next_dir):
+    # 아래 왼 위 오 순서
+    dx, dy = [1,0,-1,0], [0,-1,0,1]
+    nx, ny = x+dx[next_dir], y+dy[next_dir]
+    return mx, ny, next_dir 
+
+# 시뮬레이션 
+def simulation(x,y,move_dir):
+    # 맵 밖을 벗어나면 종료
+    while in_range(x,y):
+        # 이동횟수는 초기값은 0
+        move_num = 0
+        # (x,y) 위치의 거울이 /방향일때 처리
+        if map[x][y] = "/":
+            # 현재 방향에서 1 xor해주면 다음 위치 결정가능
+            x, y, move_dir = move(x, y, move_dir ^ 1)
+        # 거울이 \ 방향일때 처리
+        else:
+            x, y, move_dir = move(x, y, 3-move_dir)
+        # 이동횟수 1증가
+        move_num+=1
+return move_num
+
+# 시작 위치 및 방향 구하기
+x, y, move_dir = primary(start_num)
+# 시작정보들 기반으로 시뮬레이션 돌리기
+move_num = simulation(x,y, move_dir)
+# 정답출력
+print(move_num)
