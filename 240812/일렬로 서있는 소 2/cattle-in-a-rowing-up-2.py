65788a8fac9f4c74807c3cb1c7f0e242964f2049
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-12 17:28:41 +0900

    Title: 일렬로 서있는 소 2, Time: 132ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240812/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240812/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -1,9 +1,12 @@
 n = int(input())
 arr = list(map(int, input().split()))
+res = []
 cnt = 0
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
             if arr[i] < arr[j] and arr[j] < arr[k]:
-                cnt += 1
+                res.append(str(arr[i])+str(arr[j])+str(arr[k]))
+unique_res = set(res)
+cnt = len(unique_res)
 print(cnt)
