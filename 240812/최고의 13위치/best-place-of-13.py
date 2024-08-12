9a495e591271d76a500572ef118a5ce4bcb59d1d
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-12 17:55:29 +0900

    Title: 최고의 13위치, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/최고의 13위치/best-place-of-13.py
+++ 240812/최고의 13위치/best-place-of-13.py
@@ -1,11 +1,10 @@
 n = int(input())
 arr = [
-    tuple(map(int, input().split()))
+    list(map(int, input().split()))
     for _ in range(n)
 ]
 max_cnt = 0
 for i in range(n):
-    for j in range(n-1):
-        if j+1 < n and j+2 < n:
-            max_cnt = max(max_cnt, arr[i][j]+arr[i][j+1]+arr[i][j+2])
+    for j in range(n-2):
+        max_cnt = max(max_cnt, arr[i][j]+arr[i][j+1]+arr[i][j+2])
 print(max_cnt)
