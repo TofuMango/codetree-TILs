669ccb72e074ddf8867439f3f552f6428d073e7c
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-12 17:08:52 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/괄호 쌍 만들어주기 3/pair-parentheses-3.py
+++ 240812/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -4,8 +4,19 @@ n = len(arr)
 # 쌍의 수 카운팅
 match_chr = 0
 for i in range(n):
-    if arr[i] == '(':
         for j in range(i+1, n):
-            if arr[j] == ')':
+            if arr[i] == '(' and arr[j] == ')':
                 match_chr += 1
 print(match_chr)
+# 얘는 완탐이 아니군...?
+# # 입력
+# arr = tuple(input())
+# n = len(arr)
+# # 쌍의 수 카운팅
+# match_chr = 0
+# for i in range(n):
+#     if arr[i] == '(':
+#         for j in range(i+1, n):
+#             if arr[j] == ')':
+#                 match_chr += 1
+# print(match_chr)
