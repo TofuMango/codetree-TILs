c330496f3876f410f983d445803643b996ae88e7
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-13 18:34:34 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 240813/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240813/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -5,27 +5,28 @@ INT_MAX = sys.maxsize
 # 변수 선언 및 입력
 n = int(input())
 arr = [
-	list(map(int, input().split()))
-	for _ in range(n)
+    list(map(int, input().split()))
+    for _ in range(n)
 ]
 
 # 각 i번째 체크포인트를 건너 뛰었을 때의 거리를 구해줌
 ans = INT_MAX
 for i in range(1, n - 1):
-	# 거리를 구함
-	dist = 0
+    # 거리를 구함
+    dist = 0
     prev_idx = 0
-	for j in range(1, n):
-		if j == i:
-			continue
-		dist += abs(arr[prev_idx][0] - arr[j][0]) + abs(arr[prev_idx][1] - arr[j][1])
-	    # 이전 인덱스 j를 저장   
+    for j in range(1, n):
+        if j == i:
+            continue
+        dist += abs(arr[prev_idx][0] - arr[j][0]) + abs(arr[prev_idx][1] - arr[j][1])
+        # 이전 인덱스 j를 저장   
         prev_idx = j
-	
-	ans = min(ans, dist)
+    
+    ans = min(ans, dist)
 
 # 출력
 print(ans)
+
 # INT_MAX = sys.maxsize
 # min_total = INT_MAX
 # n = int(input())
