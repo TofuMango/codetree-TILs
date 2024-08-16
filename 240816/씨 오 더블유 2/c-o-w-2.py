f25bf332b06a464d04488bffb7ceee8e45e37dc9
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-16 19:11:55 +0900

    Title: 씨 오 더블유 2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240816/씨 오 더블유 2/c-o-w-2.py
+++ 240816/씨 오 더블유 2/c-o-w-2.py
@@ -1,11 +1,22 @@
+# 변수 선언 및 입력
 n = int(input())
-arr = list(input().strip())  # 문자열을 리스트로 변환 (공백 없이)
+string = input()
 cnt = 0
-
-for i in range(n - 2):
-    for j in range(i + 1, n - 1):
+for i in range(n):
+    for j in range(i + 1, n):
         for k in range(j + 1, n):
-            if arr[i] == 'C' and arr[j] == 'O' and arr[k] == 'W':
+            if string[i] == 'C' and string[j] == 'O' and string[k] == 'W':
                 cnt += 1
 
 print(cnt)
+# n = int(input())
+# arr = list(input().strip())  # 문자열을 리스트로 변환 (공백 없이)
+# cnt = 0
+
+# for i in range(n - 2):
+#     for j in range(i + 1, n - 1):
+#         for k in range(j + 1, n):
+#             if arr[i] == 'C' and arr[j] == 'O' and arr[k] == 'W':
+#                 cnt += 1
+
+# print(cnt)
