29118a7ec549da4b62844f67308f9331aed487a2
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-24 20:52:14 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240824/G or H 3/G-or-H-3.py
@@ -0,0 +1,30 @@
+max_place = 100
+# 입력
+n, k = map(int, input().split())
+people = [
+    tuple(input().split())
+    for _ in range(n)
+]
+arr = [0] * (max_place + 1)
+
+# arr 배열에 정보 저장
+last = 0
+for elem1, elem2 in people:
+    elem1 = int(elem1)
+    if elem2 == 'G':
+        elem2 = 1
+    elif elem2 == 'H':
+        elem2 = 2
+    else:
+        elem2 = 0
+    arr[elem1] = elem2
+    last = max(elem1, last)
+# 사진 크기별 최대 점수 구하는 로직
+ans = 0
+for i in range(1, last - k + 2):
+    tmp_ans = 0
+    for j in range(i, i+k+1):  # i부터 i+k-1까지
+        tmp_ans += arr[j]  # arr[j]에서 점수를 더함
+    ans = max(tmp_ans, ans)
+
+print(ans)
