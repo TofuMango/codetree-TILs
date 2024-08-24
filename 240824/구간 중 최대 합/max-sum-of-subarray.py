8cefcadb9ed48734ea6eecd9de97cded8e270d22
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-24 20:10:00 +0900

    Title: 구간 중 최대 합, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240824/구간 중 최대 합/max-sum-of-subarray.py
+++ 240824/구간 중 최대 합/max-sum-of-subarray.py
@@ -4,7 +4,7 @@ arr = list(map(int, input().split()))
 ans = 0
 # 최대합 찾는 로직
 # n이 6, k가 3이라 하면 n에다 k 빼서 0,1,2까지만 돌도록
-for i in range(n-k):
+for i in range(n-k+1):
     hap = 0
     for j in range(i, i+k):
         hap += arr[j]
