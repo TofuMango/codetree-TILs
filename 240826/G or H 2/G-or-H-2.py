3b3c07c5a8961cb58f9e541dd067d0514d345817
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-26 18:41:01 +0900

    Title: G or H 2, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 240826/G or H 2/G-or-H-2.py
+++ 240826/G or H 2/G-or-H-2.py
@@ -1,74 +1,41 @@
-MAX_NUM = 100 
+MAX_NUM = 100
 
+# 변수 선언 및 입력
 n = int(input())
-# dp 처럼 우선 만들어두기 
-arr = [0] * (MAX_NUM + 1) 
+arr = [0] * (MAX_NUM + 1)
 
 for _ in range(n):
-    x,c = input().split()
+    x, c = tuple(input().split())
     x = int(x)
 
-    if c == 'G':
-        arr[x] = 1 
-    elif c== 'H':
-        arr[x] = 2
+    # c가 G이면 1, H면 2
+    arr[x] = 1 if c == 'G' else 2
 
-max_len = 0 
-for i in range(MAX_NUM+1):
-    for j in range(i+1,MAX_NUM+1):
-        # i 와 j 위치에 사람이 있는지 확인 
-        if arr[i] == 0 or arr[j] == 0:
-            continue
-        
-        cnt_g = 0
-        cnt_h = 0
-        
-        for k in range(i,j+1):
-            if arr[k] == 1:
-                cnt_g += 1 
-            if arr[k] == 2:
-                cnt_h += 1 
-        
-        if cnt_g == 0 or cnt_h == 0 or cnt_g == cnt_h:
-            length = j - i 
-            max_len = max(max_len,length)
+# 모든 구간의 시작점 잡기
+max_len = 0
+# 시작점 설정
+for i in range(MAX_NUM + 1):
+    # i보다 j가 항상큰값이 되도록
+    # 끝점설정
+	for j in range(i + 1, MAX_NUM + 1):
+		# i와 j 위치에 사람이 있는지 확인
+		if arr[i] == 0 or arr[j] == 0:
+			continue
+		# 해당 구간 내 g와 h의 개수 구하기
+		cnt_g = 0
+		cnt_h = 0
+		# 모든 구간 탐색
+		for k in range(i, j + 1):
+            # 현재구간의 k가 1이라면 g에 1증가
+			if arr[k] == 1:
+				cnt_g += 1
+			if arr[k] == 2:
+				cnt_h += 1
+		
+		# 조건을 만족할 때 구간의 길이를 구해 최댓값과 비교
+		if cnt_g == 0 or cnt_h == 0 or cnt_g == cnt_h:
+            # j는 끝점, i는 시작점...
+			leng = j - i
+			max_len = max(max_len, leng)
 
 print(max_len)
-# max_len = 100
-# n = int(input())
-# arr = [0] * (max_len + 1)
-
-# # 바구니에 사람 수와 중요도 입력 받기
-# for _ in range(n):
-#     people, impo = input().split()
-#     people = int(people)
-#     arr[people] = impo  # arr에 'G' 또는 'H' 저장
-
-# # 최대 크기 구하기
-# max_size = 0
-
-# # 모든 구간 확인
-# for i in range(max_len + 1):
-#     for j in range(i, max_len + 1):
-#         G = 0
-#         H = 0
-#         size = 0
-        
-#         # i부터 j까지의 구간 확인
-#         for k in range(i, j + 1):
-#             if arr[k] == 'G':
-#                 G += 1
-#             elif arr[k] == 'H':
-#                 H += 1
-
-#         # 조건에 따라 size 계산
-#         if G != 0 and H == 0:
-#             size = G  # G로만 이루어져있다면
-#         elif H != 0 and G == 0:
-#             size = H  # H로만 이루어져있다면
-#         elif G == H:
-#             size = G + H  # G와 H 비율이 같다면
-
-#         max_size = max(max_size, size)
-
-# print(max_size)
