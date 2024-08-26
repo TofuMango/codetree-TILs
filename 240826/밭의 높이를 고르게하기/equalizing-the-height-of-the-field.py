8803e6c01892d064a7817be415e7c65c9a3f90e9
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-26 19:12:52 +0900

    Title: 밭의 높이를 고르게하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240826/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -0,0 +1,17 @@
+import sys
+n, h, t = map(int, input().split())
+arr = list(map(int, input().split()))
+min_ex = sys.maxsize
+
+for i in range(n-t+1):
+    exp = 0
+    for j in range(i, i+t):
+        # 현재 높이가 주어진 높이보다 작을때
+        if arr[j] < h:
+            exp += h - arr[j]
+        # 현재 높이가 주어진 높이보다 클때
+        elif arr[j] > h:
+            exp += arr[j] - h
+        
+    min_ex = min(min_ex, exp)
+print(min_ex)
