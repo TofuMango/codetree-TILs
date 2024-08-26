92d41cee102674297bbe2a1eb2147c6c4a7d21dd
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-26 16:51:29 +0900

    Title: 아름다운 수열 2, Time: 172ms, Memory: 24MB, Status: Passed - CodeTree

--- 240826/아름다운 수열 2/beautiful-sequence-2.py
+++ 240826/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,27 +1,11 @@
-# n, m = int(input())
-# arrA = list(map(int, input().split()))
-# arrB = list(map(int, input().split()))
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
 
-# 입력 받기
-N, M = map(int, input().split())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-
-# 수열 B의 각 원소의 개수를 세기
-from collections import Counter
-count_B = Counter(B)
-
-# 아름다운 수열의 개수
-beautiful_count = 0
-
-# 수열 A에서 길이가 M인 연속 부분 수열 검사
-for i in range(N - M + 1):
-    sub_sequence = A[i:i + M]
-    count_sub = Counter(sub_sequence)
-    
-    # B의 원소 개수와 일치하는지 확인
-    if count_sub == count_B:
-        beautiful_count += 1
-
-# 결과 출력
-print(beautiful_count)
+# 모든 구간의 시작점 잡기
+cnt = 0
+for i in range(0, n - m + 1):
+    if sorted(arr1[i:i+m]) == sorted(arr2):
+        cnt += 1
+print(cnt)
