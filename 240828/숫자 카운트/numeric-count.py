6cc455a2a4a3fd12e681a61d4e7b467698c4e07d
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-08-28 18:47:36 +0900

    Title: 숫자 카운트, Time: 233ms, Memory: 24MB, Status: Passed - CodeTree

--- 240828/숫자 카운트/numeric-count.py
+++ 240828/숫자 카운트/numeric-count.py
@@ -1,55 +1,54 @@
+# 변수 선언 및 입력
 n = int(input())
+arr = [
+	tuple(map(int, input().split()))
+	for _ in range(n)
+]
 
-# 입력된 질문과 카운트 정보를 저장
-queries = []
-for _ in range(n):
-    num, count1, count2 = tuple(map(int, input().split()))
-    queries.append((str(num), count1, count2))
-
-# 가능한 모든 세 자리 수 조합을 생성
-possible_numbers = []
-
+# 모든 숫자 다 만들어봄
+cnt = 0
 for i in range(1, 10):
     for j in range(1, 10):
         for k in range(1, 10):
-            if i != j and j != k and i != k:  # 서로 다른 숫자 조건
-                possible_numbers.append(str(i) + str(j) + str(k))
-
-def count_matches(secret, guess):
-    exact_count = 0
-    non_exact_count = 0
-    
-    # 정확한 자리 비교
-    for i in range(3):
-        if secret[i] == guess[i]:
-            exact_count += 1
-            
-    # 다른 자리에서의 숫자 비교
-    for digit in guess:
-        if digit in secret:
-            # 다른 자리에서 일치하는 경우
-            if secret[guess.index(digit)] != digit:
-                non_exact_count += 1
+            # 같은 수가 있는지 확인하기
+            # 서로 다른 수로 구성되어야하기때문
+            if i == j or j == k or k == i:
+                continue
+            # 만약 해당 숫자가 정답이라면 모든 입력에 대해 만족하는지 확인
+            succeeded = True
+            for a, num_cnt1, num_cnt2 in arr:
+                # a의 각 자리별 숫자 뜯기
+                # 백의자리
+                x = a // 100
+                # 십의자리
+                y = a // 10 % 10
+                # 일의자리
+                z = a % 10
                 
-    return exact_count, non_exact_count
-
-# 가능한 숫자 중에서 유효한 숫자 카운트
-valid_count = 0
-
-for number in possible_numbers:
-    is_valid = True
-    for query in queries:
-        guess = query[0]
-        count1 = query[1]
-        count2 = query[2]
-        
-        exact_count, non_exact_count = count_matches(number, guess)
-        
-        if exact_count != count1 or non_exact_count != count2:
-            is_valid = False
-            break
-            
-    if is_valid:
-        valid_count += 1
-
-print(valid_count)
+                # 1번카운트, 2번카운트 초기화
+                cnt1 = 0
+                cnt2 = 0
+                # 주어진 숫자와 같고 동일한 위치에 자리할때
+                if x == i:
+                    cnt1 += 1
+                if y == j:
+                    cnt1 += 1
+                if z == k:
+                    cnt1 += 1
+                # 주어진 숫자와 같고 위치는 다르지만 세자리에 해당하는 숫자가 있을때
+                # 백의자리는 십의자리/일의자리 숫자 중 하나와 동일한경우
+                if x == j or x == k:
+                    cnt2 += 1
+                if y == i or y == k:
+                    cnt2 += 1
+                if z == i or z == j:
+                    cnt2 += 1
+                
+                # 만약 직접 카운트한것과 주어진 카운트 수가 다르다면
+                # 해당숫자는 정답이 될수없음
+                if cnt1 != num_cnt1 or cnt2 != num_cnt2:
+                    succeeded = False
+                    break
+            if succeeded:
+                cnt += 1
+print(cnt)
