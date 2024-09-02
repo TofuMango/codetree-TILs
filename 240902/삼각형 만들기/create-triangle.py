d0479df3cd3395af3c5441d941087490e96d8b6b
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-09-02 19:21:20 +0900

    Title: 삼각형 만들기, Time: 108ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240902/삼각형 만들기/create-triangle.py
+++ 240902/삼각형 만들기/create-triangle.py
@@ -2,8 +2,7 @@ n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 
 def calculate_area(x1, y1, x2, y2, x3, y3):
-    # 삼각형의 넓이를 계산합니다.
-    return abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2
+    return abs((x1 - x2) * (y3 - y1))//2
 
 max_area = 0
 
@@ -17,21 +16,21 @@ for i in range(n):
 
             # (x1, y1)와 (x2, y2)로 x축에 평행한 변을 만든 경우
             if y1 == y2:  # x축에 평행한 변
-                if y3 != y1:  # y축에 평행한 변
+                if y3 != y1:  # y축에 평행한 변을 만들 수 있는지 확인
                     area = calculate_area(x1, y1, x2, y2, x3, y3)
                     max_area = max(max_area, area)
 
             # (x2, y2)와 (x3, y3)로 x축에 평행한 변을 만든 경우
             if y2 == y3:  # x축에 평행한 변
-                if y1 != y2:  # y축에 평행한 변
+                if y1 != y2:  # y축에 평행한 변을 만들 수 있는지 확인
                     area = calculate_area(x1, y1, x2, y2, x3, y3)
                     max_area = max(max_area, area)
 
             # (x1, y1)와 (x3, y3)로 x축에 평행한 변을 만든 경우
             if y1 == y3:  # x축에 평행한 변
-                if y2 != y1:  # y축에 평행한 변
+                if y2 != y1:  # y축에 평행한 변을 만들 수 있는지 확인
                     area = calculate_area(x1, y1, x2, y2, x3, y3)
                     max_area = max(max_area, area)
 
 # 결과 출력
-print(int(max_area * 2))  # 넓이에 2를 곱해서 출력
+print(max_area * 2)  # 넓이에 2를 곱해서 출력
